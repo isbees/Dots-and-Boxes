0a5dc9921139387018184c468befaf22653f0e34
isbees <isbeesbrukhman@gmail.com>
2017-05-18 08:19:28 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,19 @@
+
+public class Game {
+private int[][] dimenions;
+private int boxes;
+private int player1, player2;
+
+public Game(){
+}
+
+public Game(int r, int c){
+dimenions = new int[r][c];
+}
+
+public int count(){
+for(int i = 0; i < dimenions.length; i++){
+  for(int j = 0; j < dimenions[0].length; j++){
+  if(i)
+  }
+}
