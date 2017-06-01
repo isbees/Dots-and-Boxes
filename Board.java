f509f139eb2e071bd9f1d1f6627cb2051c844b4a
Hasan Muhammad <hsnmuh47@gmail.com>
2017-06-01 19:10:37 -0400

    Added line drawing functionality to Game
    Click two points to draw a line between them

--- Board.java
+++ Board.java
@@ -1,21 +1,25 @@
 import javafx.application.*;
+import javafx.event.EventHandler.*;
+import javafx.event.*;
+import java.util.*;
 import javafx.stage.*;
 import javafx.scene.*;
-import javafx.scene.canvas.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
-import javafx.scene.text.*;
 
 
 public class Board extends Application {
 	private Circle[][] board;
+	private ArrayList<Point> theClicked;
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("Dots And Boxes v 0.0.1 Beta"); //Stage contains everything
+		stage.setTitle("Dahrts And Burxes v 0.1.0"); //Stage contains everything
 		Group root = new Group();
 		stage.setWidth(375);
 		stage.setHeight(375);
 		board = new Circle[4][4];
+		theClicked = new ArrayList<Point>();
 		int rows = 4 * 100;
 		int columns = 4 * 100;
 		int r = 0;
@@ -35,12 +39,76 @@ public class Board extends Application {
 		}
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
+		scene.setOnMouseClicked
+		( 
+			new EventHandler<MouseEvent>()
+			{
+				public void handle(MouseEvent e)
+				{
+					for(int r = 0; r < board.length; r++)
+					{
+						for(int c = 0; c < board[0].length; c++)
+						{
+							if(board[r][c].contains(e.getX(), e.getY()))
+							{
+								theClicked.add(new Point(board[r][c].getCenterX(), board[r][c].getCenterY()));
+							}
+						}
+					}
+					Line ln = checkIfPaired();
+					if(ln.getStartX() != 0)
+					{
+						root.getChildren().add(ln);
+					}
+				}
+			}
+		);
 		stage.show();
 	}
 	
 	public static void main(String[] args) {
 		launch(args); //Launches the start method above
 	}
+	
+	public Line checkIfPaired()
+	{
+		Line ln;
+		if(theClicked.size() == 2)
+		{
+			int x1 = (int) theClicked.get(0).getX();
+			int y1 = (int) theClicked.get(0).getY();
+			int x2 = (int) theClicked.get(1).getX();
+			int y2 = (int) theClicked.get(1).getY();
+			ln = new Line(x1,y1,x2,y2);
+			theClicked.remove(0);
+			theClicked.remove(0);
+		}
+		else
+		{
+			ln = new Line();
+		}
+		return ln; //CHANGE IMMEDIATELY
+	}
 }
 
+class Point
+{
+	private double x;
+	private double y;
+	public Point(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public double getX(){return x;}
+	public double getY(){return y;}
+	public boolean equals(Point otherPoint)
+	{
+		if(this.getX() == otherPoint.getX() && this.getY() == otherPoint.getY())
+		{
+			return true;
+		}
+		return false;
+	}
+}
 
